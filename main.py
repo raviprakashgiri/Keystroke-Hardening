2d4cf1c9f0d1a1d97245f7590ae0c32bdb4a9e1e
raviprakashgiri <raviprakashgiri@gmail.com>
2016-10-14 15:25:17 -0400

    main created

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import sys
+import os
+
+import numpy as np
+import argparse
+
+import random
+from simplecrypt import encrypt, decrypt, DecryptionException
+from Crypto.Hash import SHA256
+import Crypto
+
+
+# standard deviations: par_k
+std_val = 2
+# mean: Par_t
+mean_val = 10
+
+q_val = Crypto.Util.number.getPrime(160, randfunc=None)
+
+# fixed file size as asked
+h_history_file_size = 500
+history_file_name = 'hpd'
+# number of features
+h_max_entries = 8
